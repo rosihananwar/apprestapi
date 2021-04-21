ba918de5b13440305be3131334c00c9ed947bb3d
rosihananwar <rosi.mandala@gmail.com>
2021-04-21 15:24:50 +0700

    registrasi user

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 
 const express = require('express');
 //const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(morgan('dev'));
 
 //app.use(bodyParser.urlencoded({extended:true}));
 //app.use(bodyParser.json());
 
+// panggil routes
 var routes = require('./routes');
     routes(app);
+// daftarkan menu route dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
