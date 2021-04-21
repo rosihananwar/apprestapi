571e52555724b3566b5992f0566f651b7af77df1
rosihananwar <rosi.mandala@gmail.com>
2021-04-21 12:36:41 +0700

    jwt intall

--- routes.js
+++ routes.js
@@ -17,5 +17,7 @@ module.exports = function(app){
     app.route('/ubah')
         .put(jsonku.ubahMahasiswa);
     app.route('/hapus')
-        .delete(jsonku.hapusMahasiswa)
+        .delete(jsonku.hapusMahasiswa);
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatakuliah);
     }
